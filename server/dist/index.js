4bab964bb9c2b53827719621112b87e2752f825a
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-04-22 11:17:11 +0530

    basic setup on client side

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,17 @@
-"use strict";
-console.log("hello world");
+import express from "express";
+import cors from "cors";
+import newServer from "http";
+import { Socket, io, socket } from "socket.io";
+const app = express();
+const socketServer = new newServer(app);
+const httpServer = socketServer.create(Socket);
+app.use(cors, httpServer, {
+    origin: "http://localhost:5173",
+    credentials: true
+});
+io.on("connection", () => {
+    socket.emit("chat", "start the chat server");
+    console.log("server successfuly connected ");
+});
+const path = 8000;
+httpServer.listen(path, () => console.log("server is listening to us"));
